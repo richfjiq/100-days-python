50638d54481128d1e6cc76f0eee3da64ab8fc499
richfjiq <rfjiq1986@gmail.com>
2023-07-19 17:02:55 -0600

    chore: day-18 - draw spirograph

---
+++ day-18/spirograph.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+tim.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+def draw_circle(deg):
+    tim.circle(100)
+    tim.setheading(deg)
+
+
+def draw_spirograph(gap):
+    degrees = 0
+
+    while degrees <= 360:
+        tim.color(random_color())
+        draw_circle(degrees)
+        degrees += gap
+
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
