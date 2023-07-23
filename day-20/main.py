969cacc0e85267b805be92232e70ac1b6a07c9da
richfjiq <rfjiq1986@gmail.com>
2023-07-22 20:52:17 -0600

    chore: day-21 - detect snake collision with wall and tail, game completed

--- day-20/main.py
+++ day-20/main.py
@@ -27,8 +27,27 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreBoard.increase_score()
+
+    # Detect collision with wall
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() > 280
+    ):
+        game_is_on = False
+        scoreBoard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreBoard.game_over()
+
 
 screen.exitonclick()
--- day-20/score.py
+++ day-20/score.py
@@ -21,3 +21,7 @@ class ScoreBoard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
