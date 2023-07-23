c0a9c8ae3f3ba614c5dfb4159e83242659c11710
richfjiq <rfjiq1986@gmail.com>
2023-07-22 20:20:27 -0600

    chore: day-21 - add Food and Scoreboard classes

--- day-20/main.py
+++ day-20/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from score import ScoreBoard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreBoard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,7 +25,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreBoard.increase_score()
 
 screen.exitonclick()
