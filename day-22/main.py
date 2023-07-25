95e9f2375d217af65d00980b1ae03dcf74819bf0
richfjiq <rfjiq1986@gmail.com>
2023-07-25 17:58:20 -0600

    chore: day-22 - pong game, add paddle class, left and right paddle

--- day-22/main.py
+++ day-22/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 # 1. Create the screen
 screen = Screen()
@@ -8,34 +9,20 @@ screen.title("Pong")
 screen.tracer(0)
 
 # 2. Create and move a paddle
-paddle = Turtle("square")
-paddle.setheading(90)
-paddle.penup()
-paddle.color("white")
-paddle.shapesize(stretch_wid=1, stretch_len=5)
-paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+paddle_left = Paddle((-350, 0))
+# 3. Create another paddle
+paddle_right = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(paddle_left.go_up, "w")
+screen.onkey(paddle_left.go_down, "s")
+screen.onkey(paddle_right.go_up, "Up")
+screen.onkey(paddle_right.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
     screen.update()
 
-
-# 3. Create another paddle
 # 4. Create the ball and make it move
 # 5. Detect collision with wall and bounce
 # 6. Detect collision with paddle
