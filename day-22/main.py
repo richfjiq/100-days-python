b49dddce1ec0bbec03aff88859a4823422ed3911
richfjiq <rfjiq1986@gmail.com>
2023-07-25 20:29:06 -0600

    chore: day-22 - pong game, detect wall and paddle collisions

--- day-22/main.py
+++ day-22/main.py
@@ -14,6 +14,7 @@ screen.tracer(0)
 # 3. Create another paddle
 paddle_left = Paddle((-350, 0))
 paddle_right = Paddle((350, 0))
+# 4. Create the ball and make it move
 ball = Ball()
 
 screen.listen()
@@ -27,10 +28,18 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+    # 5. Detect collision with wall and bounce
+    ball.collision_wall()
+    # 6. Detect collision with paddle
+    if (
+        ball.distance(paddle_right) < 50
+        and ball.xcor() > 320
+        or ball.distance(paddle_left) < 50
+        and ball.xcor() < -320
+    ):
+        print("Made contact")
+        ball.bounce_x()
 
-# 4. Create the ball and make it move
-# 5. Detect collision with wall and bounce
-# 6. Detect collision with paddle
 # 7. Detect when paddle misses
 # 8. Keep score
 
