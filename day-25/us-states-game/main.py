ffa57aaf0747da1861493b499a64f20048561205
richfjiq <rfjiq1986@gmail.com>
2023-08-01 21:38:52 -0600

    chore: day-26 - dictionary comprehension exercises

--- day-25/us-states-game/main.py
+++ day-25/us-states-game/main.py
@@ -22,13 +22,11 @@ while len(guessed_states) < 50:
     ).title()
 
     if answer_state == "Exit":
-        missing_states = []
-
-        for state in all_states:
-            if state in guessed_states:
-                pass
-            else:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
--- day-26/main.py
+++ day-26/main.py
@@ -15,7 +15,26 @@
 
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) < 5]
-print(short_names)
+# print(short_names)
 
 long_names = [name.upper() for name in names if len(name) > 5]
-print(long_names)
+# print(long_names)
+
+# Dictionary comprehension
+
+import random
+
+# new_dict = { new_key: new_value for item in list }
+# new_dict = { new_key: new_value for (key, value) in dict.items() }
+# new_dict = { new_key: new_value for (key, value) in dict.items() if test }
+
+# new_dict = { new_key: new_value for item in list }
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+students_score = {student: random.randint(1, 100) for student in names}
+print(students_score)
+
+# new_dict = { new_key: new_value for (key, value) in dict.items() }
+passed_students = {
+    student: score for (student, score) in students_score.items() if score >= 60
+}
+print(passed_students)
