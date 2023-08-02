b26ac66973ea154bb13da4e30cd927d4decd7f8a
richfjiq <rfjiq1986@gmail.com>
2023-08-01 22:16:57 -0600

    chore: day-26 - nato alphabet exercise

--- day-26/main.py
+++ day-26/main.py
@@ -31,10 +31,33 @@ import random
 # new_dict = { new_key: new_value for item in list }
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 students_score = {student: random.randint(1, 100) for student in names}
-print(students_score)
+# print(students_score)
 
 # new_dict = { new_key: new_value for (key, value) in dict.items() }
 passed_students = {
     student: score for (student, score) in students_score.items() if score >= 60
 }
-print(passed_students)
+# print(passed_students)
+
+
+# Iterate over Pandas DataFrame
+
+student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
+
+# Looping through dictionaries:
+# for key, value in student_dict.items():
+#     print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+# Loop through a data frame
+# for key, value in student_data_frame.items():
+#     print(value)
+
+# Loop through rows of a data frame
+for index, row in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
