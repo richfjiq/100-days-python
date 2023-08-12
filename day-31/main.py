7a53d8a97daaf4d603323b10a4821344292dd7f3
richfjiq <rfjiq1986@gmail.com>
2023-08-11 22:24:40 -0600

    chore: day-31 - create new card

--- day-31/main.py
+++ day-31/main.py
@@ -1,7 +1,20 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-FONT = ("Arial", 24)
+FONT = ("Arial", 40, "italic")
+FONT_BOLD = ("Arial", 60, "bold")
+
+# ---------------------------------------- Pick random word ----------------------------------------
+data = pandas.read_csv("data/french_words.csv")
+words_dict = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(words_dict)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=f"{current_card['French']}")
 
 
 window = Tk()
@@ -9,23 +22,30 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
+
 canvas.create_image(400, 263, image=card_front)
+card_title = canvas.create_text(400, 150, text="Title", font=FONT)
+card_word = canvas.create_text(400, 263, text="word", font=FONT_BOLD)
 canvas.grid(column=0, row=0, columnspan=2)
 
-language_label = Label(text="French", font=("Arial", 40, "italic"), bg="white")
-language_label.place(x=400, y=150, anchor="center")
-word_label = Label(text="trouve", font=("Arial", 40, "italic"), bg="white")
-word_label.place(x=400, y=263, anchor="center")
 
 wrong_image = PhotoImage(file="images/wrong.png")
 wrong_button = Button(
-    image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR
+    image=wrong_image,
+    highlightthickness=0,
+    highlightbackground=BACKGROUND_COLOR,
+    command=next_card,
 )
 wrong_button.grid(column=0, row=1)
 right_image = PhotoImage(file="images/right.png")
 right_button = Button(
-    image=right_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR
+    image=right_image,
+    highlightthickness=0,
+    highlightbackground=BACKGROUND_COLOR,
+    command=next_card,
 )
 right_button.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
