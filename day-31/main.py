dfea3f834638388128aa6fea95ab586a874763c6
richfjiq <rfjiq1986@gmail.com>
2023-08-11 22:55:19 -0600

    chore: day-31 - add flip cards

--- day-31/main.py
+++ day-31/main.py
@@ -6,24 +6,36 @@ BACKGROUND_COLOR = "#B1DDC6"
 FONT = ("Arial", 40, "italic")
 FONT_BOLD = ("Arial", 60, "bold")
 
-# ---------------------------------------- Pick random word ----------------------------------------
 data = pandas.read_csv("data/french_words.csv")
 words_dict = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(words_dict)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=f"{current_card['French']}")
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=f"{current_card['French']}", fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back)
 
 
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
-
-canvas.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front)
 card_title = canvas.create_text(400, 150, text="Title", font=FONT)
 card_word = canvas.create_text(400, 263, text="word", font=FONT_BOLD)
 canvas.grid(column=0, row=0, columnspan=2)
