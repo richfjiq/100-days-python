7b76862d57fa9adaf5ee50c87f0cc9f1aaa429ef
richfjiq <rfjiq1986@gmail.com>
2023-09-23 18:58:08 -0600

    day 38 - workout tracking: sheety set up and authenticate sheety api

--- day-38/main.py
+++ day-38/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 APP_ID = "14e098eb"
 API_KEY = "53bb2651eaddd9d9d38f9ed48cd5cd3c"
@@ -21,9 +22,32 @@ params = {
     "age": AGE,
 }
 
+today = datetime.now()
+date = today.strftime("%d/%m/%Y")
+time = today.time().strftime("%H:%M:%S")
 
 response = requests.post(url=nutritionixt_endpoint, headers=headers, json=params)
 response.raise_for_status()
-
 data = response.json()
-print(data)
+
+sheety_endpoint = (
+    "https://api.sheety.co/9be0467a8435c8ea87080d818f18feac/workoutTracking/workouts"
+)
+SHEETY_USERNAME = "rich86workouts"
+SHEETY_PASSWORD = "dB2d}9XR0/p]"
+
+basic = requests.auth.HTTPBasicAuth(SHEETY_USERNAME, SHEETY_PASSWORD)
+
+for item in data["exercises"]:
+    body = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": item["user_input"].title(),
+            "duration": item["duration_min"],
+            "calories": item["nf_calories"],
+        }
+    }
+    response = requests.post(url=sheety_endpoint, auth=basic, json=body)
+    response.raise_for_status()
+    print("Successfully posted :)")
