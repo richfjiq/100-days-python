1b4703c1cc2bb361d6a3523f02864ab86e157dc1
richfjiq <rfjiq1986@gmail.com>
2023-10-26 15:49:18 -0600

    day-54 - pyhton decorator functions

--- day-54/hello.py
+++ day-54/hello.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ day-54/main.py
@@ -0,0 +1,31 @@
+## Python Decorator Function
+import time
+
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        # Do something before
+        function()
+        function()
+        # Do something after
+
+    return wrapper_function
+
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+def say_greeting():
+    print("How are you?")
+
+
+decorated_function = delay_decorator(say_greeting)
+decorated_function()
