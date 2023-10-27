90afcfce4703a8a5d046f041c57846e9c6e8f7b6
richfjiq <rfjiq1986@gmail.com>
2023-10-26 23:11:22 -0600

    day-55 - Flask: advanced decorators exercise

---
+++ day-55/exercise.py
@@ -0,0 +1,20 @@
+# TODO: Create the logging_decorator() function 👇
+def logging_decorator(function):
+    def wrapper(*args):
+        print(f"You called {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+
+    return wrapper
+
+
+# TODO: Use the decorator 👇
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+
+
+inputs = [1, 2, 3]
+a_function(inputs[0], inputs[1], inputs[2])
