05b36fe5d3f2c7d2ab7bb844840b2efe1272c971
richfjiq <rfjiq1986@gmail.com>
2023-10-26 18:50:17 -0600

    day-55 - Flask: url paths and debugger

---
+++ day-55/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name}, you are {number} years old!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
