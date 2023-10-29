f19808a6a14bffe027df7bb4a40ab08d6bd6f004
richfjiq <rfjiq1986@gmail.com>
2023-10-29 15:23:10 -0600

    day-57 - Flask: combining Jinja templating with APIs

--- day-57/server.py
+++ day-57/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def home():
     random_number = random.randint(1, 10)
     current_year = datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    age_response = requests.get(f"https://api.agify.io/?name={name}")
+    gender_response = requests.get(f"https://api.genderize.io/?name={name}")
+    age = age_response.json()["age"]
+    gender = gender_response.json()["gender"]
+    return render_template("guess.html", name=name.capitalize(), age=age, gender=gender)
 
 
 if __name__ == "__main__":
