79fce40735e82f0d65b0c45370a303ade8016bf6
richfjiq <rfjiq1986@gmail.com>
2023-11-08 05:29:52 -0600

    day-60 - Flask: handle POST requests with Flask server

--- day-60/main.py
+++ day-60/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+    return render_template("login.html", email=email, password=password)
 
 
 if __name__ == "__main__":
