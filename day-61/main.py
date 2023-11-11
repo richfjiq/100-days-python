88426dd7a1e3b6e9c94a3ff2ed2c5fe142930d32
richfjiq <rfjiq1986@gmail.com>
2023-11-10 21:09:52 -0600

    day-61 - Advanced Forms: creating forms with Flask-WTF

--- day-61/main.py
+++ day-61/main.py
@@ -1,20 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
-"""
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-"""
+class LoginForm(FlaskForm):
+    email = StringField("Email")
+    password = StringField("Password")
 
 
 app = Flask(__name__)
+app.secret_key = "ZeWHKvkoqhEiehMjW5kYeCiE4ecAdshPRje55K6u"
 
 
 @app.route("/")
@@ -24,7 +19,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == "__main__":
