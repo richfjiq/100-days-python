7d19195e4ba712b669db95afa2ed4b745fbaad2f
richfjiq <rfjiq1986@gmail.com>
2023-11-10 22:13:11 -0600

    day-61 - Advanced Forms: adding validations to forms with Flask-WTF

--- day-61/main.py
+++ day-61/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email")
-    password = StringField("Password")
+    email = EmailField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        label="Password",
+        validators=[DataRequired(), Length(min=8)],
+    )
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
@@ -17,9 +22,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template("login.html", form=login_form)
 
 
