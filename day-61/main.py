e4eed8e92938ecd50c1d2f3bb24a507ddaf4283c
richfjiq <rfjiq1986@gmail.com>
2023-11-14 21:55:17 -0600

    day-61 - Flask-WTForms: using Bootstrap-Flask

--- day-61/main.py
+++ day-61/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 
 class LoginForm(FlaskForm):
@@ -15,6 +16,7 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "ZeWHKvkoqhEiehMjW5kYeCiE4ecAdshPRje55K6u"
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
