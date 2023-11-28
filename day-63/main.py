b29b032a0e6d748c7d6e6f4181c181d90ce79f7f
richfjiq <rfjiq1986@gmail.com>
2023-11-27 21:42:37 -0600

    day-63 - Init project Virtual Bookshelf: databases with sqlite

--- day-63/main.py
+++ day-63/main.py
@@ -3,6 +3,9 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, VARCHAR, Float, update
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 """
 Red underlines? Install the required packages first:
@@ -17,11 +20,30 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 """
 
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 Bootstrap5(app)
+db.init_app(app)
 
 all_books = []
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[VARCHAR] = mapped_column(VARCHAR(250), unique=True, nullable=False)
+    author: Mapped[VARCHAR] = mapped_column(VARCHAR(250), nullable=False)
+    rating: Mapped[Float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"self.title"
 
 
 class BookForm(FlaskForm):
@@ -33,10 +55,27 @@ class BookForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+class UpdateRatingForm(FlaskForm):
+    new_rating = SelectField(
+        "New Rating", choices=("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+    )
+    submit = SubmitField("Submit")
+
+
 @app.route("/")
 def home():
-    print(f"All books: {all_books}")
-    return render_template("index.html", books=all_books)
+    books = db.session.execute(db.select(Book).order_by(Book.title)).scalars().all()
+
+    return render_template("index.html", books=books)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id")
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -44,16 +83,38 @@ def add():
     form = BookForm()
 
     if form.validate_on_submit():
-        book_data = {
-            "title": form.name.data,
-            "author": form.author.data,
-            "rating": int(form.rating.data),
-        }
+        with app.app_context():
+            db.create_all()
 
-        all_books.append(book_data)
+        with app.app_context():
+            book = Book(
+                title=form.name.data,
+                author=form.author.data,
+                rating=int(form.rating.data),
+            )
+            db.session.add(book)
+            db.session.commit()
 
         return redirect(url_for("home"))
     return render_template("add.html", form=form)
+
+
+@app.route("/edit/<int:num>", methods=["GET", "POST"])
+def edit(num):
+    form = UpdateRatingForm()
+    book = db.get_or_404(Book, num)
+
+    if form.validate_on_submit():
+        with app.app_context():
+            book_to_update = db.session.execute(
+                db.select(Book).where(Book.id == num)
+            ).scalar()
+            book_to_update.rating = form.new_rating.data
+            db.session.commit()
+
+        return redirect(url_for("home"))
+
+    return render_template("edit.html", form=form, book=book)
 
 
 if __name__ == "__main__":
