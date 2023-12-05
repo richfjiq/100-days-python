9c8a2aa636458ade70b142cd9649326daafa61b4
richfjiq <rfjiq1986@gmail.com>
2023-12-04 21:54:42 -0600

    day-66 - REST API: adds get request to get a random cafe

--- day-66/main.py
+++ day-66/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from random import choice
 
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
+"""
+Install the required packages first:
+Open the Terminal in PyCharm (bottom left).
 
 On Windows type:
 python -m pip install -r requirements.txt
@@ -12,12 +13,12 @@ On MacOS type:
 pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
-'''
+"""
 
 app = Flask(__name__)
 
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -36,6 +37,17 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+
+        return dictionary
+
 
 with app.app_context():
     db.create_all()
@@ -44,6 +56,28 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/random")
+def get_random():
+    cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    random_cafe = choice(cafes)
+    # return jsonify(
+    #     cafe={
+    #         "id": random_cafe.id,
+    #         "name": random_cafe.name,
+    #         "map_url": random_cafe.map_url,
+    #         "img_url": random_cafe.img_url,
+    #         "location": random_cafe.location,
+    #         "has_sockets": random_cafe.has_sockets,
+    #         "has_toilet": random_cafe.has_toilet,
+    #         "has_wifi": random_cafe.has_wifi,
+    #         "can_take_calls": random_cafe.can_take_calls,
+    #         "seats": random_cafe.seats,
+    #         "coffee_price": random_cafe.coffee_price,
+    #     }
+    # )
+    return jsonify(cafe=random_cafe.to_dict())
 
 
 # HTTP GET - Read Record
@@ -55,5 +89,5 @@ def home():
 # HTTP DELETE - Delete Record
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
