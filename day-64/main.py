b6c9487b0d307624d2338865242b01238d948875
richfjiq <rfjiq1986@gmail.com>
2023-12-04 19:54:59 -0600

    day-64 - My top 100 Movies Website: be able to sort and rank the movies by rating

--- day-64/main.py
+++ day-64/main.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
-from sqlalchemy import Integer, VARCHAR, Float
+from sqlalchemy import Integer, VARCHAR, Float, asc, desc
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 import os
 from dotenv import load_dotenv
@@ -49,7 +49,7 @@ class Movie(db.Model):
     year: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[VARCHAR] = mapped_column(VARCHAR(350), nullable=False)
     rating: Mapped[Float] = mapped_column(Float, nullable=False)
-    ranking: Mapped[Integer] = mapped_column(Integer, nullable=False)
+    ranking: Mapped[VARCHAR] = mapped_column(VARCHAR(10), nullable=False)
     review: Mapped[VARCHAR] = mapped_column(VARCHAR(250), nullable=False)
     image_url: Mapped[VARCHAR] = mapped_column(VARCHAR(250), nullable=False)
 
@@ -67,18 +67,18 @@ class AddForm(FlaskForm):
 
 @app.route("/")
 def home():
-    # # create the database
+    # create the database
     # with app.app_context():
     #     db.create_all()
 
-    # # adds two movies to the database
+    # adds two movies to the database
     # with app.app_context():
     #     new_movie1 = Movie(
     #         title="Phone Booth",
     #         year=2002,
     #         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
     #         rating=7.3,
-    #         ranking=10,
+    #         ranking="10",
     #         review="My favourite character was the caller.",
     #         image_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg",
     #     )
@@ -87,16 +87,22 @@ def home():
     #         year=2022,
     #         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
     #         rating=7.3,
-    #         ranking=9,
+    #         ranking="9",
     #         review="I liked the water.",
     #         image_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
     #     )
     #     db.session.add_all([new_movie1, new_movie2])
     #     db.session.commit()
 
-    movies = (
-        db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars().all()
-    )
+    movies = db.session.execute(db.select(Movie).order_by(desc(Movie.rating))).scalars().all()
+
+    for index in range(len(movies)):
+        print(movies[index])
+        movie_to_update = db.session.execute(
+            db.select(Movie).where(Movie.id == movies[index].id)
+        ).scalar()
+        movie_to_update.ranking = f"{index + 1}"
+        db.session.commit()
 
     return render_template("index.html", movies=movies)
 
@@ -163,7 +169,7 @@ def find():
             year=data["release_date"][0:4],
             description=data["overview"],
             rating=0,
-            ranking=0,
+            ranking="None",
             review="None",
             image_url=f"https://image.tmdb.org/t/p/w500/{data['poster_path']}",
         )
