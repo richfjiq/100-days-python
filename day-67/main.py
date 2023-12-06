7fe19b466b62c76b6bf5106f9af6e86229c21249
richfjiq <rfjiq1986@gmail.com>
2023-12-06 13:25:22 -0600

    day-67 - RESTful blogs API: adds delete blog posts

--- day-67/main.py
+++ day-67/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -132,6 +132,28 @@ def edit_post(id):
 
 
 # TODO: delete_post() to remove a blog post from the database
+@app.route("/delete")
+def delete():
+    post_id = request.args.get("post_id")
+    print(post_id)
+    try:
+        post_to_delete = db.get_or_404(BlogPost, post_id)
+    except:
+        return (
+            jsonify(
+                error=(
+                    {
+                        "Not Found": "Sorry a post with that id was not found in the database."
+                    }
+                )
+            ),
+            404,
+        )
+        return redirect(url_for("get_all_posts"))
+    else:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
 
 
 # Below is the code from previous lessons. No changes needed.
