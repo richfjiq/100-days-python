84ff3bd687eef7f11a5edced2a561ed0774c54ba
richfjiq <rfjiq1986@gmail.com>
2023-12-15 23:01:18 -0600

    day-69 - Blog Capstone Project: allow any user to add comments to BlogPosts

--- day-69/main.py
+++ day-69/main.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 
 # Import your forms from the forms.py
-from forms import CreatePostForm, RegisterForm, LoginForm
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 
 """
@@ -30,6 +30,16 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap5(app)
+gravatar = Gravatar(
+    app,
+    size=100,
+    rating="g",
+    default="retro",
+    force_default=False,
+    force_lower=False,
+    use_ssl=False,
+    base_url=None,
+)
 
 # TODO: Configure Flask-Login
 login_manager = LoginManager()
@@ -68,13 +78,14 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # Create Foreign Key, "users.id" the users refers to the tablename of User.
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    # Create reference to the User object, the "posts" refers to the posts protperty in the User class.
+    # Create reference to the User object, the "posts" refers to the posts property in the User class.
     author = relationship("User", back_populates="posts")
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+    comments = relationship("Comment", back_populates="blog_post")
 
 
 # TODO: Create a User table for all your registered users.
@@ -87,6 +98,17 @@ class User(UserMixin, db.Model):
     # This will act like a List of BlogPost objects attached to each User.
     # The "author" refers to the author property in the BlogPost class.
     posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="user")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = relationship("User", back_populates="comments")
+    blog_post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    blog_post = relationship("BlogPost", back_populates="comments")
 
 
 with app.app_context():
@@ -169,15 +191,35 @@ def get_all_posts():
 
 
 # TODO: Allow logged-in users to comment on posts
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
+    form = CommentForm()
     requested_post = db.get_or_404(BlogPost, post_id)
+    result = db.session.execute(
+        db.select(Comment).where(Comment.blog_post_id == post_id)
+    )
+    comments = result.scalars().all()
+
+    if form.validate_on_submit():
+        if current_user.is_authenticated:
+            new_comment = Comment(
+                text=form.comment.data, user=current_user, blog_post=requested_post
+            )
+            db.session.add(new_comment)
+            db.session.commit()
+            return redirect(url_for("show_post", post_id=requested_post.id))
+        else:
+            flash("Please log in to post a comment.")
+            return redirect(url_for("login"))
+
     is_admin = is_admin_fn()
     return render_template(
         "post.html",
         post=requested_post,
         logged_in=current_user.is_authenticated,
         is_admin=is_admin,
+        form=form,
+        comments=comments,
     )
 
 
